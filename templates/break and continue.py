747a1a9bc51190ef10a02d6c50c6fa6cd5b53fd1
Khushi Singh <itskhushi.singh4545@gmail.com>
2025-08-15 13:45:07 +0530

    Update style.css for square story cards and 10 per row

---
+++ templates/break and continue.py
@@ -0,0 +1,14 @@
+for i in range (12):
+    if (i==10):
+        
+        print("leave loop")   
+        continue
+    print( "5 X ", i+1, "=", 5*(i+1) )
+        
+for i in range (12):
+    if (i==10):
+        
+        print("stop loop")   
+        break
+    print( "5 X ", i+1, "=", 5*(i+1) )
+                
